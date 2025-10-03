3ddc32e98c1af63ac7bcd83ce34d1d76b668a4d1
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-10-03 19:59:37 -0300

    Create list component.

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -21,15 +21,103 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { map, startWith, switchMap } from 'rxjs/operators';
+import { MoviesService } from '../../services/movies.service';
+import MoviesResponse from '../../models/movies';
+import Movie from '../../models/movies';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatTableModule, MatPaginatorModule, MatProgressSpinnerModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class ListComponent {
+export class ListComponent implements AfterViewInit {
 
+  displayedColumns: string[] = ['id', 'year', 'title', 'winner'];
+  dataSource!: MatTableDataSource<Movie>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  totalElements = 0;
+  isLoadingResults = true;
+  year = 0;
+  winner = undefined;
+
+  constructor(private moviesService: MoviesService) { }
+
+  ngAfterViewInit() {
+
+    this.loadMovies(0);
+
+    this.paginator.page.pipe(
+      startWith({}),
+      switchMap(() => {
+        this.isLoadingResults = true;
+        return this.moviesService.getMovies(this.paginator.pageIndex, this.winner, this.year);
+      }),
+      map((response: MoviesResponse) => {
+        this.isLoadingResults = false;
+        this.totalElements = response.totalElements;
+        return response.content;
+      }),
+    ).subscribe(content => {
+      this.dataSource = new MatTableDataSource(content);
+    });
+  }
+
+  public filterByYear(event: Event) {
+
+    const value = (event.target as HTMLInputElement).value;
+
+    const regex = /^\d{4}$/;
+
+    if (regex.test(value)) {
+
+      this.year = parseInt(value);
+
+      this.loadMovies(0, this.winner, this.year);
+
+      this.goToFirstPage();
+
+    } else if (value.length == 0) {
+
+      this.year = 0;
+
+      this.loadMovies(0, this.winner, this.year);
+
+      this.goToFirstPage();
+    }
+  }
+
+  public filterByWinner(event: MatSelectChange) {
+
+    const winner = event.value;
+    this.winner = (winner !== undefined) ? winner : undefined;
+    this.loadMovies(0, this.winner, this.year);
+  }
+
+  private loadMovies(page: number, winner?: boolean, year?: number): void {
+
+    this.moviesService.getMovies(page, winner, year).subscribe((response: MoviesResponse) => {
+      this.dataSource = new MatTableDataSource(response.content);
+      this.dataSource.paginator = this.paginator;
+      this.totalElements = response.totalElements;
+      this.isLoadingResults = false;
+    });
+  }
+
+  private goToFirstPage(): void {
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
