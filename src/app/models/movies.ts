4c5d810356d463be7afa384a5102303494aa836e
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-10-03 19:13:21 -0300

    Create a type for movies response.

--- src/app/models/movies.ts
+++ src/app/models/movies.ts
@@ -30,3 +30,31 @@ export default interface Movie {
     winner: boolean
 }
 
+interface Sort {
+    unsorted: boolean;
+    sorted: boolean;
+    empty: boolean;
+}
+
+interface Pageable {
+    pageNumber: number;
+    pageSize: number;
+    sort: Sort;
+    offset: number;
+    unpaged: boolean;
+    paged: boolean;
+}
+
+export default interface MoviesResponse {
+    content: Movie[];
+    pageable: Pageable;
+    totalPages: number;
+    totalElements: number;
+    last: boolean;
+    numberOfElements: number;
+    size: number;
+    number: number;
+    sort: Sort;
+    first: boolean;
+    empty: boolean;
+}
