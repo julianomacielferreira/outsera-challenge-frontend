be716bc8bc22c070ca9c6492cfc073526d8112b4
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-10-03 15:40:19 -0300

    Create dashboard component.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -21,15 +21,78 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MoviesService } from '../../services/movies.service';
+import YearResponse from '../../models/year-response';
+import WinnnersCount from '../../models/year-response';
+import StudioWinners from '../../models/studio-winners';
+import Studio from '../../models/studio-winners';
+import IntervalForProducers from '../../models/producers-response';
+import Producer from '../../models/producers-response';
+import Movie from '../../models/movies';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatTableModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  public winnnersCountColumns: string[] = ['year', 'winnerCount'];
+  public winnnersCountDataSource!: Array<WinnnersCount>;
+
+  public studioWinnersColumns: string[] = ['name', 'winCount'];
+  public studioWinnersDataSource!: Array<Studio>;
+
+  public producersMinIntervalWinsColumns: string[] = ['producer', 'interval', 'previousWin', 'followingWin'];
+  public producersMinIntervalWinsDataSource !: Array<Producer>;
+
+  public producersMaxIntervalWinsColumns: string[] = ['producer', 'interval', 'previousWin', 'followingWin'];
+  public producersMaxIntervalWinsDataSource  !: Array<Producer>;
+
+  public moviesListByYearColumns: string[] = ['id', 'year', 'title'];
+  public moviesListByYearDataSource !: Array<Movie>;
+
+  constructor(private moviesService: MoviesService) { }
+
+  ngOnInit() {
+
+    this.moviesService.getYearsWithMultipleWinners().subscribe((response: YearResponse) => {
+      this.winnnersCountDataSource = response.years;
+    });
+
+    this.moviesService.getStudioWinners().subscribe((response: StudioWinners) => {
+      this.studioWinnersDataSource = response.studios.slice(0, 3);
+    });
+
+    this.moviesService.getMaxMinWinIntervalForProducers().subscribe((response: IntervalForProducers) => {
+      this.producersMinIntervalWinsDataSource = response.min;
+      this.producersMaxIntervalWinsDataSource = response.max;
+    });
+  }
+
+  public loadMovieWinnersByYear(event: Event) {
+
+    const year = (event.target as HTMLInputElement).value;
+
+    const regex = /^\d{4}$/;
+
+    if (regex.test(year)) {
+
+      this.moviesService.getMovieWinnersByYear(parseInt(year)).subscribe((response: Array<Movie>) => {
+        this.moviesListByYearDataSource = response;
+      });
+
+    } else if (year.length == 0) {
+
+      this.moviesListByYearDataSource = [];
+    }
+  }
 }
