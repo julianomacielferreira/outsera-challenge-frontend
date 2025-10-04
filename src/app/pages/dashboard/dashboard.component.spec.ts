db65b7d01f6352abd4a5139807146f62525b8396
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-10-04 07:35:55 -0300

    Dashboard component tests.

--- src/app/pages/dashboard/dashboard.component.spec.ts
+++ src/app/pages/dashboard/dashboard.component.spec.ts
@@ -21,26 +21,75 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { DashboardComponent } from './dashboard.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MoviesService } from '../../services/movies.service';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+  let fixture: any;
+  let moviesService: MoviesService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
-    })
-      .compileComponents();
-
-    fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      imports: [
+        MatCardModule,
+        MatTableModule,
+        MatInputModule,
+        MatFormFieldModule,
+        HttpClientTestingModule,
+        BrowserAnimationsModule,
+        DashboardComponent
+      ],
+      providers: [MoviesService]
+    }).compileComponents();
   });
 
-  it('should create', () => {
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+    moviesService = TestBed.inject(MoviesService);
+  });
+
+  it('deve criar o componente', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve carregar os dados de anos com múltiplos vencedores', () => {
+    const response: any = { years: [{ year: 2020, winnerCount: 2 }] };
+    spyOn(moviesService, 'getYearsWithMultipleWinners').and.returnValue(of(response));
+
+    fixture.detectChanges();
+    expect(component.winnnersCountDataSource).toEqual(response.years);
+  });
+
+  it('deve carregar os dados de estúdios vencedores', () => {
+    const response: any = { studios: [{ name: 'Estúdio 1', winCount: 2 }, { name: 'Estúdio 2', winCount: 1 }] };
+    spyOn(moviesService, 'getStudioWinners').and.returnValue(of(response));
+    fixture.detectChanges();
+    expect(component.studioWinnersDataSource).toEqual(response.studios.slice(0, 3));
+  });
+
+  it('deve carregar os dados de produtores com intervalo mínimo e máximo de vitórias', () => {
+    const response: any = { min: [{ producer: 'Produtor 1', interval: 2, previousWin: 2020, followingWin: 2022 }], max: [{ producer: 'Produtor 2', interval: 5, previousWin: 2015, followingWin: 2020 }] };
+    spyOn(moviesService, 'getMaxMinWinIntervalForProducers').and.returnValue(of(response));
+    fixture.detectChanges();
+    expect(component.producersMinIntervalWinsDataSource).toEqual(response.min);
+    expect(component.producersMaxIntervalWinsDataSource).toEqual(response.max);
+  });
+
+  it('deve carregar os dados de filmes vencedores por ano', () => {
+    const response: any = [{ id: 1, year: 2020, title: 'Filme 1' }];
+    spyOn(moviesService, 'getMovieWinnersByYear').and.returnValue(of(response));
+    const event: any = { target: { value: '2020' } };
+    component.loadMovieWinnersByYear(event);
+    expect(component.moviesListByYearDataSource).toEqual(response);
   });
 });
